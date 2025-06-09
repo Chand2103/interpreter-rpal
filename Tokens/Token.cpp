50b901e33dea2aecf2f7d1d5863f2f7d515f39c8
Chand2103 <chandupaambepitiya@gmail.com>
2025-06-09 21:30:01 +0530

    changes in tokens

--- Tokens/Token.cpp
+++ Tokens/Token.cpp
@@ -1,9 +1,10 @@
-/*
- * Token.cpp
- *
- *  Created on: Oct 14, 2011
- *      Author: Saurabh Saxena
- *      // UFID        : 21817195
+/**
+ * Token Implementation File - Token Class Methods
+ * 
+ * This implementation provides the Token class functionality for representing
+ * various types of tokens in functional programming language evaluation.
+ * The class supports multiple constructor patterns for different token types
+ * including simple values, lambda closures, environments, and conditional constructs.
  */
 
 #include "Token.h"
@@ -11,46 +12,79 @@
 
 using namespace std;
 
+/**
+ * Initialize tuple vector - sets up internal data structure
+ * This method initializes the tuple vector container for the token
+ */
+void Token::construct() {
+    this->tuple = vector<Token>();
+}
+
+/**
+ * Default constructor - initializes empty token
+ * Creates a basic token with default values and tuple initialization
+ */
 Token::Token() {
-	// TODO Auto-generated constructor stub
-	isTuple = false;
-	construct();
+    isTuple = false;
+    construct();
 }
 
-Token::Token(std::string value, std::string type){
-	this->value = value;
-	this->type = type;
-	isTuple = false;
-	construct();
+/**
+ * Lambda closure constructor - creates token for lambda expressions
+ * @param type - token type identifier
+ * @param lambdaParam - parameter name for lambda function
+ * @param lambdaNum - unique identifier number for lambda
+ */
+Token::Token(std::string type, std::string lambdaParam, int lambdaNum) {
+    this->type = type;
+    this->lambdaParam = lambdaParam;
+    this->lambdaNum = lambdaNum;
+    isTuple = false;
+    construct();
 }
 
-Token::Token(std::string type, int envNum){
-	this->type = type;
-	this->envNum = envNum;
-	isTuple = false;
-	construct();
+/**
+ * Environment constructor - creates token for environment context
+ * @param type - token type identifier  
+ * @param envNum - unique environment identifier number
+ */
+Token::Token(std::string type, int envNum) {
+    this->type = type;
+    this->envNum = envNum;
+    isTuple = false;
+    construct();
 }
 
-void Token::construct(){
-	this->tuple = vector<Token>();
+/**
+ * Simple token constructor - creates basic value token
+ * @param value - actual content/value of the token
+ * @param type - token type classification
+ */
+Token::Token(std::string value, std::string type) {
+    this->value = value;
+    this->type = type;
+    isTuple = false;
+    construct();
 }
 
-Token::Token(std::string type,std::string lambdaParam, int lambdaNum){
-	this->type = type;
-	this->lambdaParam = lambdaParam;
-	this->lambdaNum = lambdaNum;
-	isTuple = false;
-	construct();
+/**
+ * Conditional constructor - creates token for beta reduction
+ * @param type - token type identifier
+ * @param betaIfDeltaNum - delta number for if branch
+ * @param betaElseDeltaNum - delta number for else branch  
+ */
+Token::Token(std::string type, int betaIfDeltaNum, int betaElseDeltaNum) {
+    this->type = type;
+    this->betaIfDeltaNum = betaIfDeltaNum;
+    this->betaElseDeltaNum = betaElseDeltaNum;
+    isTuple = false;
+    construct();
 }
 
-Token::Token(std::string type, int betaIfDeltaNum, int betaElseDeltaNum){
-	this->type = type;
-	this->betaIfDeltaNum = betaIfDeltaNum;
-	this->betaElseDeltaNum = betaElseDeltaNum;
-	isTuple = false;
-	construct();
-}
-
+/**
+ * Destructor - cleans up token resources
+ * Virtual destructor for proper cleanup in inheritance scenarios
+ */
 Token::~Token() {
-	// TODO Auto-generated destructor stub
+    // Token cleanup handled automatically by vector destructor
 }
--- Tokens/Token.h
+++ Tokens/Token.h
@@ -1,10 +1,12 @@
-/*
- * Token.h
- *
- *  Created on: Oct 14, 2011
- *      Author: Saurabh Saxena
- *      // UFID        : 21817195
+/**
+ * Token Header File - Token Class Definition
+ * 
+ * This header defines the Token class which represents various types of tokens
+ * used in functional programming language evaluation. The Token class supports
+ * different token types including simple values, lambda closures, environments,
+ * and conditional constructs, providing a flexible structure for language processing.
  */
+
 #include <string>
 #include <vector>
 
@@ -13,24 +15,64 @@
 
 class Token {
 public:
-	Token();
-	Token(std::string value, std::string type); //Simple token constructor
-	Token(std::string type,std::string lamdaParam, int lamdaNum);// Constructor for lambda closure
-	Token(std::string type, int envNum ); // Constructor for environment
-	Token(std::string type, int betaIfDeltaNum, int betaElseDeltaNum);
-	virtual ~Token();
-	std::string value;
-	std::string type;
-	int lambdaNum;
-	std::string lambdaParam;
-	int envNum;
-	int betaIfDeltaNum;
-	int betaElseDeltaNum;
-	int tauCount;
-	bool isTuple;
-	std::vector<Token> tuple;
-	void construct();
-	int lambdaEnv;
+    /**
+     * Default constructor - initializes empty token
+     */
+    Token();
+    
+    /**
+     * Lambda closure constructor - creates token for lambda expressions
+     * @param type - token type identifier
+     * @param lamdaParam - parameter name for lambda
+     * @param lamdaNum - lambda identifier number
+     */
+    Token(std::string type, std::string lamdaParam, int lamdaNum);
+    
+    /**
+     * Environment constructor - creates token for environment context
+     * @param type - token type identifier
+     * @param envNum - environment identifier number
+     */
+    Token(std::string type, int envNum);
+    
+    /**
+     * Simple token constructor - creates basic value token
+     * @param value - token value content
+     * @param type - token type identifier
+     */
+    Token(std::string value, std::string type);
+    
+    /**
+     * Conditional constructor - creates token for beta reduction
+     * @param type - token type identifier
+     * @param betaIfDeltaNum - if branch delta number
+     * @param betaElseDeltaNum - else branch delta number
+     */
+    Token(std::string type, int betaIfDeltaNum, int betaElseDeltaNum);
+    
+    /**
+     * Destructor - cleans up token resources
+     */
+    virtual ~Token();
+    
+    /**
+     * Initialize tuple vector - sets up internal data structure
+     */
+    void construct();
+    
+    // Token data members
+    std::string value;              // Token value content
+    std::string type;               // Token type identifier
+    int lambdaNum;                  // Lambda identifier number
+    std::string lambdaParam;        // Lambda parameter name
+    int envNum;                     // Environment identifier
+    int betaIfDeltaNum;            // Beta if branch number
+    int betaElseDeltaNum;          // Beta else branch number
+    int tauCount;                   // Tau count for tuples
+    bool isTuple;                   // Tuple flag indicator
+    std::vector<Token> tuple;       // Tuple container
+    int lambdaEnv;                  // Lambda environment reference
+
 };
 
 #endif /* TOKEN_H_ */
